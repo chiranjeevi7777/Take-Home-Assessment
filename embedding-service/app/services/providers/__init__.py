ac607509c58a1d582246ce14e41ecea7ac3096b4
Developer <dev@github.com>
2026-08-25 09:33:48 +0530

    Clean workspace for SDE Intern Assessment project

--- embedding-service/app/services/providers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import BaseEmbeddingProvider
-from .sentence_transformer import SentenceTransformerProvider
-from .openai_provider import OpenAIProvider
-
-__all__ = ["BaseEmbeddingProvider", "SentenceTransformerProvider", "OpenAIProvider"]
